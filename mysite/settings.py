1b4fafa8ae3cc6ba76d9b43d8568eae0194fbefb
Kwantz <erickkwantantz123@gmail.com>
2019-05-27 15:29:41 +0700

    update setting

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,9 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-CORS_ORIGIN_WHITELIST = (
-    'localhost:3000'
-)
+CORS_ORIGIN_ALLOW_ALL = True
+
+# CORS_ORIGIN_WHITELIST = (
+#     'localhost:3000'
+# )
 
 # Application definition
 

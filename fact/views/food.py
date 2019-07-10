dc60b39fdd61bdec4c75ca311ece948131386a68
Kwantz <erickkwantantz123@gmail.com>
2019-07-10 15:27:12 +0700

    Debug

--- fact/views/food.py
+++ fact/views/food.py
@@ -65,7 +65,6 @@ def api_food(request):
 
         json_request = json.loads(request.body)
         have_data = Food.objects.annotate(lower_name=Lower("name")).filter(lower_name__exact = json_request["name"].lower()).values('id', 'name')
-        return JsonResponse({"message": json_request["name"] + " is already available in database.", "debug": list(have_data)}, status=400)
 
         if len(have_data) > 0:
             return JsonResponse({"message": json_request["name"] + " is already available in database.", "debug": list(have_data)}, status=400)

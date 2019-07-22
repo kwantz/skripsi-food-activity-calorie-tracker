a029632ef1afe22c5a3081de5cf1a90e17095b3c
Kwantz <erickkwantantz123@gmail.com>
2019-07-22 19:26:45 +0700

    Change Random

--- fact/views/comparison.py
+++ fact/views/comparison.py
@@ -41,7 +41,7 @@ def api_comparison(request):
         train_label = get_train_labels()
         train_feature = get_train_features(user.id)
 
-        random.seed(2)
+        random.seed(3)
         random.shuffle(train_feature)
 
         X = train_feature[:, 1:]

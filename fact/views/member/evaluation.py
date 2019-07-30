f9c3b5b1109fbe9c1c166f9531799b57d5e53801
Kwantz <erickkwantantz123@gmail.com>
2019-07-30 22:46:35 +0700

    Add Evaluation

---
+++ fact/views/member/evaluation.py
@@ -0,0 +1,39 @@
+from fact.models import Quote, Article, CalorieIntake, MealDetail, ActivityLevel, CalorieBurnt
+from django.http import JsonResponse
+from django.db.models import F, Sum
+from fact.libraries.jwt import JWT
+from fact.libraries.body import clasify_activity_factor
+from datetime import datetime, timedelta, time, date
+from fact.libraries.body import calculate_bmr
+
+
+def api_member_evaluation(request):
+    bearer, token = request.META.get('HTTP_AUTHORIZATION').split()
+    user = JWT().decode(token)
+
+    if user is None:
+        return JsonResponse({"message": "Unauthorized"}, status=401)
+
+    if request.method == "GET":
+        today = datetime.now()
+        total_calorie = 0
+
+        for i in range(30):
+            date_start = datetime.combine(today + timedelta((i + 0) * -1), time())
+            date_end = datetime.combine(today + timedelta((i - 1) * -1), time())
+            calorie_burnt = CalorieBurnt.objects.filter(user=user, created_at__gte=date_start, created_at__lte=date_end, deleted_at__isnull=True)
+
+            month_calorie = 0
+            for calorie in calorie_burnt:
+                month_calorie += calorie.activity_label.met * user.weight * calorie.duration / 3600
+
+            total_calorie += (month_calorie / len(calorie_burnt))
+
+        total_calorie = (total_calorie / 30)
+
+        result = clasify_activity_factor(total_calorie)
+        return JsonResponse({
+            "results": result
+        })
+    
+    return JsonResponse({"message": "Not Found"}, status=404)
